b6f40d6e6b028bdc8bd24a1d0661c875892ba01e
zimbatm <zimbatm@zimbatm.com>
2017-03-05 16:39:34 +0000

    Pin the other dependencies
    Make sure the test only downloads the `%03d.txt` files.
    
    Show proper benchmark display

--- default.nix
+++ default.nix
@@ -1,20 +1,46 @@
 let
-  pkgsSrc = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/1f9bd9246b1ea5af599a94a5104f3f207322be3e.tar.gz";
+  nixpkgs = (import <nixpkgs> {}).srcOnly {
+    name = "nixpkgs";
+    src = import <nix/fetchurl.nix> {
+      url = "https://github.com/NixOS/nixpkgs/archive/1f9bd9246b1ea5af599a94a5104f3f207322be3e.tar.gz";
+      sha256 = "005ax3r50z2czlnpyshggxcnbg2dpxiwasr7w1yri1bqvnnqhi0g";
+    };
   };
-  pkgs = import pkgsSrc {};
-  inherit (pkgs) lib stdenv fetchurl;
 
-  fixtures = lib.take 100 (lib.importJSON ./fixtures.json);
-  paths = builtins.map (pair: toString (fetchurl pair)) fixtures;
+  pkgs = import nixpkgs {};
+
+  inherit (pkgs) lib stdenv fetchurl buildEnv;
+  #fetchurl = import <nix/fetchurl.nix>;
+
 in
-stdenv.mkDerivation {
-  name = "nix-parallel-test-case";
-  src = null;
+  {
+    deps = buildEnv {
+      name = "deps";
+      paths = with pkgs; [
+        nixpkgs
+        stdenv
 
-  phases = ["installPhase"];
+        curl curl.dev curl.man
+        libev
+        libssh2 libssh2.dev
+        nghttp2 nghttp2.dev
+        openssl openssl.dev
+        perl
+      ];
+    };
+    test =
+      let
+        fixtures = lib.take 100 (lib.importJSON ./fixtures.json);
+        fixturePaths = builtins.map (pair: toString (fetchurl pair)) fixtures;
 
-  installPhase = ''
-    echo ${toString paths} > $out
-  '';
-}
+      in
+      stdenv.mkDerivation {
+        name = "nix-parallel-test-case";
+        src = null;
+        phases = ["installPhase"];
+        # Just create links to all the fixture derivations
+        installPhase = ''
+            echo ${toString fixturePaths} > $out
+        '';
+      };
+  }
